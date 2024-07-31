7234318a4024a238caafa742e3c99630a916bebc
flavienlankai@gmail.com <flavienlankai@gmail.com>
2024-07-31 23:04:59 +0200

    oublie changement adresse

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    //baseURL: 'http://35.181.169.48:80',
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://35.181.169.48:80',
+    //baseURL: 'http://localhost:8080',
 });
 
 export default api;
